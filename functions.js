651231a51cd41a9c5d25c6da255ee461b1168b56
Shpaner <Shpaner@users.noreply.github.com>
2020-11-15 22:19:15 +0100

    delete bugs

--- functions.js
+++ functions.js
@@ -1,128 +1,175 @@
-// autor Tobiasz Witalis
+// autor - Tobiasz Witalis
 
-const signsArray = ['/', '*', '-', '+']
-const specialSignsArray = ['sin', 'cos', 'tan', 'sqrt', 'x^2', 'arcsin']
-var isSign
-var changeValue
-var inputs
-var lastCharacter
-var lastCharacterIsSign
-var giveResult = 0
-var clearButton
-var justCalculated
+var equation = ""
+var wasSymbol
+var chosenSymbol = "/"
+var lastNumber = "0"
+var convert = true
+var hasCalculated = true
+var label
+var equ_label
+
+// calculate equation
+function calculateEquation(equ) {
+    var lastChar = equ.slice(-1)
+    
+    // if is not a number
+    if (!(lastChar >= '0' && lastChar <= '9')) {
+    
+        // remove last character
+        equ = equ.slice(0, -1)
+    }
+    return eval(equ)
+}
+
+// manipulatie number-buttons
+function nrButton(btnNr) {
+    
+    changeColor(chosenSymbol)
+
+    if (hasCalculated) {
+        // add and replace new number
+        console.log("1")
+        label.value = btnNr
+        lastNumber = btnNr
+        equation = ""
+        equ_label.value = "" // change visibility
+    } else {
+        // add number at the end
+        var lastChar = equ_label.value.slice(-1)
+        if ((lastChar >= '0' && lastChar <= '9') || lastChar == "") /*&& (label.value.length < 9)*/ {
+            label.value += btnNr
+            lastNumber += btnNr
+        } else {
+            if (wasSymbol) {
+                label.value = btnNr
+            } else {
+                label.value += btnNr
+                lastNumber += btnNr
+            }
+        }
+    }
+    wasSymbol = false
+    hasCalculated = false
+}
+
+// +, -, *, /
+function actionButton(btnSymbol) {
+
+    changeColor(btnSymbol)
+
+    if (!hasCalculated) {
+        if (!wasSymbol) {
+            console.log("3")
+            equation += label.value + btnSymbol
+            equ_label.value = equation
+            lastNumber = label.value
+            label.value = calculateEquation(equation)
+            chosenSymbol = btnSymbol
+        } else {
+            console.log("4")
+            var tmp = equ_label.value.slice(0, -1)
+            equation = tmp + btnSymbol
+            equ_label.value = equation
+            chosenSymbol = btnSymbol
+        }
+    } else {
+        console.log("5")
+        chosenSymbol = btnSymbol
+        lastNumber = label.value
+        equation = lastNumber + chosenSymbol
+        equ_label.value = equation
+    }
+
+    hasCalculated = false
+    wasSymbol = true
+}
 
 function addValue(val) {
 
-	if (justCalculated) {
-		clearValues()
-		justCalculated = false
-	}
-
-	lastCharacter = inputs[1].value.slice(-1)
-	lastCharacterIsSign = false
-	isSign = false
-	isSpecialSign = false
-	console.log(giveResult)
-
-	for (var i = 0; i < signsArray.length; i++)  {
-
-		// sprawdz czy nowy val jest znakiem
-		if (signsArray[i] == val)
-			isSign = true
-
-		// sprawdz czy ostatni charakter jest symbolem
-		if (signsArray[i] == lastCharacter)
-			lastCharacterIsSign = true
-	}
-
-	for (var i = 0; i < specialSignsArray.length; i++) {
+    const signsArray = ['/', '*', '-', '+']
+    const specialSignsArray = ['sin', 'cos', 'tan', 'sqrt', 'x^2', 'arcsin']
+    var isSymbol = false
+    var isSpecialSign = false
+    
+    for (var i = 0; i < specialSignsArray.length; i++) {
 		if (specialSignsArray[i] == val) {
 			isSpecialSign = true
 			break
 		}
 	}
 
-	if (isSign) {
+    for (var i = 0; i < signsArray.length; i++) {
+        if (val == signsArray[i]) {
+            // symbol button
+            actionButton(val)
+            isSymbol = true
+            break
+        }
+    }
 
-		// +, + or -, -, etc (doubled the val) - nothing happens
-		if (lastCharacter == val) {
-			console.log("1")
-			changeValue = true
-		}
+    // number button
+    if (!isSymbol && !isSpecialSign) {
+        nrButton(val)
+    }
 
-		// podmianka
-		else if (lastCharacterIsSign) {
-
-			if (giveResult < 2) {
-				console.log("2")
-				inputs[1].value = inputs[1].value.slice(0, -1) + val
-				changeColor(val)
-			} 
-
-			else {
-				inputs[1].value = inputs[1].value + inputs[0].value + val
-				inputs[0].value = eval(inputs[1].value.slice(0, -1))
-				giveResult -= 1
-			}
-			changeValue = true
-		}
-
-		// dodaj znak na koncu
-		else if (!lastCharacterIsSign) {
-			console.log("3")
-			changeColor(val)
-			inputs[1].value += inputs[0].value + val
-			changeValue = true
-			giveResult += 1
-		}
-	}
-	
-	// normalne liczby (nie jest znakiem)
-	else if (!isSpecialSign) {
-		if (!changeValue) {
-			console.log("4")
-			inputs[0].value += val // dodaj liczbe
-		} else {
-			console.log("5")
-			inputs[0].value = val // zresetuj i dodaj liczbe
-			giveResult += 1
-		}
-		changeValue = false
-	}
-
-	// znaki specjalne
-	else {
-
-		console.log("special sign")
-		if (inputs[0].value != "") {
+    // special sign
+    else {
+		if (label.value != "") {
 
 			if (val == 'sin')
-				inputs[0].value = Math.sin(inputs[0].value)
+                label.value = Math.sin(label.value)
 			else if (val == 'cos')
-				inputs[0].value = Math.cos(inputs[0].value)
+                label.value = Math.cos(label.value)
 			else if (val == 'tan')
-				inputs[0].value = Math.tan(inputs[0].value)
+                label.value = Math.tan(label.value)
 			else if (val == 'sqrt')
-				inputs[0].value = Math.sqrt(inputs[0].value)
+                label.value = Math.sqrt(label.value)
 			else if (val == 'x^2')
-				inputs[0].value = Math.pow(inputs[0].value, 2)
+                label.value = Math.pow(label.value, 2)
 			else if (val == 'arcsin')
-				inputs[0].value = Math.asin(inputs[0].value)
+                label.value = Math.asin(label.value)
 		}
-	}
+    }
 }
 
-function changeColor(val) {
+// onclick "=" button
+function calculateValue() {
 
-	if (lastCharacter != "") {
-		// change color of previously clicked sign
-		document.getElementById(lastCharacter).style.color = "#FFF"
-		document.getElementById(lastCharacter).style.backgroundColor = "#0c2835"
-	}
+    changeColor(chosenSymbol)
 
-	//change color of the chosen sign
-	document.getElementById(val).style.color = "#111"
-	document.getElementById(val).style.backgroundColor = "#0091ea"
+    if (!wasSymbol) {
+        if (chosenSymbol == '/' && label.value == '') {
+            label.value = "Błąd"
+            convert = false
+        } else {
+            lastNumber = label.value
+            equation += label.value
+        }
+    } else {
+        equation = label.value + chosenSymbol + lastNumber
+    }
+
+    if (convert) {
+        equ_label.value = equation
+        label.value = calculateEquation(equation)
+    }
+
+    wasSymbol = true
+    hasCalculated = true
+    convert = true
+}
+
+// onClick "c" (clear) button
+function clearValues() {
+    changeColor(chosenSymbol)
+    label.value = ""
+    equation = ""
+    wasSymbol = true
+    chosenSymbol = "/"
+    lastNumber = "0"
+    equ_label.value = ""
+    hasCalculated = true
 }
 
 // open special options signs
@@ -138,36 +185,30 @@ function openSpec() {
 	}
 }
 
-function calculateValue() {
+function changeColor(val) {
+
+    //change color of the chosen sign
+    document.getElementById(val).style.color = "#111"
+    document.getElementById(val).style.backgroundColor = "#0091ea"
+
+	// change color of previously clicked sign
+	document.getElementById(chosenSymbol).style.color = "#FFF"
+    document.getElementById(chosenSymbol).style.backgroundColor = "#0c2835"
 	
-	inputs[1].value += inputs[0].value
-	if (!lastCharacterIsSign)
-		inputs[0].value = eval(inputs[1].value.slice(0, -1))
-	else 
-		inputs[0].value = eval(inputs[1].value)
-
-	justCalculated = true
-}
-
-// clear button
-function clearValues() {
-
-	console.log("clear")
-	inputs.forEach( input => {
-    	input.value = ""
-  	})
 }
 
 // on open change input to none
 document.addEventListener('DOMContentLoaded', function() {
 
-	inputs = document.querySelectorAll('input')
+	var inputs = document.querySelectorAll('input')
 	inputs.forEach( input => {
     	input.value = ""
-  	}) 
+    })
+     
+    label = inputs[0]
+    equ_label = inputs[1]
 
   	spans = document.querySelectorAll('span')
   	clearButton = spans[0]
 
 }, false)
-
